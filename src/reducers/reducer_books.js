c076e7449c018f72c5bff0396f4528a6290e233e
Matt Warren <mwarren@harding.edu>
2017-09-04 15:20:45 -0400

    create reducer books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'JavaScript: The Good Parts' },
+		{ title: 'Eloquent Ruby' },
+		{ title: 'Jack Reacher' },
+		{ title: 'Clifford the Red Dog' }
+	]
+}
