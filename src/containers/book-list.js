66d48d5a7b3fd74fc6c142ead0d2ac57617b8690
Matt Warren <mwarren@harding.edu>
2017-09-16 16:39:21 -0400

    establish connection between redux and react with connect function

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-export default class BookList extends Component {
+class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
-				<li key={book.title} className="list-group-item">{book.title}</li>
+				<li key={book.title} className="list-group-item">
+					{book.title}
+				</li>
 			);
 		});
 	}
@@ -16,3 +19,12 @@ export default class BookList extends Component {
 		)
 	}
 }
+
+// connection between redux and react
+function mapStateToProps(state) {
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
